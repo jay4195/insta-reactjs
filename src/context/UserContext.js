5ab122627b094b4949f3ae0591b47f86b0c42495
ZHIYUAN WANG(IT) <jay4195@qq.com>
2021-08-23 10:18:08 +0800

    user localstorage bug fixed

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -3,6 +3,11 @@ import React, { useState, createContext } from "react";
 export const UserContext = createContext(null);
 
 export const UserProvider = ({ children }) => {
+  var localStorageUser = localStorage.getItem("user");
+  if (localStorageUser === "" || localStorageUser === "undefined") {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+  }
   const localSt = JSON.parse(localStorage.getItem("user"));
   const [user, setUser] = useState(localSt ? localSt : null);
 
